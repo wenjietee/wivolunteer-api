adf3931127c672807f8091d7f95f83944e83d21d
jesstoh <jesstoh23@gmail.com>
2021-03-28 19:04:15 +0800

    Login Route working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,31 @@
 // DEPENDENCIES
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
+const jsonwebtoken = require("jsonwebtoken");
 
 //ROUTES
 
-// New login with setting of json token at local storage
-router.get('/login', (req, res) => {
-	res.send('new login');
+// New login with sending json web token to front-end
+router.post("/login", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, foundUser) => {
+        if (!foundUser) {
+            res.status(404).json({ error: "no user found" });
+        } else {
+            // Check if password is correct
+            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+                res.json(foundUser);
+            } else {
+                res.status(401).json({ error: "wrong password" });
+            }
+        }
+    });
 });
 
 // Logout - remove local storage json token
-router.delete('/logout', (req, res) => {
-	res.send('logout');
+router.delete("/logout", (req, res) => {
+    res.send("logout");
 });
 
 // EXPORT
