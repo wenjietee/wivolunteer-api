3852b2851ecaa526a079da101bfbee7aa6179c14
jesstoh <jesstoh23@gmail.com>
2021-03-29 23:06:49 +0800

    Change user model to include events user interested in

--- models/user.js
+++ models/user.js
@@ -1,18 +1,19 @@
 // DEPENDENCIES
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // SCHEMA
 const userSchema = new mongoose.Schema({
-	email: { type: String, unique: true, required: true },
-	username: { type: String, unique: true, required: true },
-	password: { type: String, required: true },
-	contact: String,
-	interests: [String],
-	pastEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
-	address: String,
-	image: String,
+    email: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    contact: String,
+    interests: [String],
+    pastEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+    interestedEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+    address: String,
+    image: String,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
