9782c779c2af978f991fb9ab03904f4938960a34
Wenjie <wenjietj@gmail.com>
2021-03-29 17:21:40 +0800

    add feedback get, create routes

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const Event = require('../models/event.js');
+const User = require('../models/user.js');
 const Feedback = require('../models/feedback.js');
 
 //ROUTES
 
 // Fetch feedback info for the event
 router.get('/feedback/:id', (req, res) => {
-	res.send('fetch feedback');
+	Feedback.find({ event: req.params.id })
+		.populate('event', 'eventTitle')
+		.populate('participant', 'username')
+		.exec()
+		.then((feedbacks) => {
+			if (!feedbacks) {
+				return res.status(404).json({
+					error: 'No feedback avaliable.',
+				});
+			}
+			res.status(200).json(feedbacks);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+			});
+		});
 });
 
 // Create a new feedback for the event
 router.post('/feedback/:id', (req, res) => {
-	res.send('create a feedback');
+	// Set participant as current user
+	req.body.participant = req.user._id;
+	// Set event as params id
+	req.body.event = req.params.id;
+	// Create feedback
+	Feedback.create(req.body, (err, createdFeedback) => {
+		if (err) res.status(500).json({ error: err });
+		else {
+			res.status(201).json(createdFeedback);
+		}
+	});
 });
 
 // EXPORT
