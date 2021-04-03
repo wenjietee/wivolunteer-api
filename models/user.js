6854f2a9ccf08b2574ede612c39fa4d9d834e3ca
Wenjie <wenjietj@gmail.com>
2021-04-03 10:27:43 +0800

    update user model with default image

--- models/user.js
+++ models/user.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 
 // SCHEMA
 const userSchema = new mongoose.Schema({
-    email: { type: String, unique: true, required: true },
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    contact: String,
-    interests: [String],
-    pastEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
-    interestedEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
-    address: String,
-    image: String,
+	email: { type: String, unique: true, required: true },
+	username: { type: String, unique: true, required: true },
+	password: { type: String, required: true },
+	contact: String,
+	interests: [String],
+	pastEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+	interestedEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+	address: String,
+	image: {
+		type: String,
+		default:
+			"https://cdn.pixabay.com/photo/2017/02/10/12/12/volunteer-2055042_1280.png",
+	},
 });
 
 const User = mongoose.model("User", userSchema);
